f7517fe652f2bce74ae34e8a11fb5cbf71ff73e9
RodrigoMattosoSilveira <r3850365@gmail.com>
2025-11-09 17:22:48 -0800

    Fixed a cache problem

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"path/filepath"
 )
 
 func main() {
@@ -12,34 +13,62 @@ func main() {
 
 	// Serve static assets if you have them (optional)
 	r.Static("/static", "./static")
-
-	// Load all templates
-	r.SetHTMLTemplate(loadTemplates("./templates"))
-
+	// Helper: renders a view with layout and partials
+	render := func(c *gin.Context, tmpl string, data gin.H) {
+		files := []string{
+			filepath.Join("templates", "layout.html"),
+			filepath.Join("templates", "header.html"),
+			filepath.Join("templates", "sidebar.html"),
+			filepath.Join("templates", "footer.html"),
+			filepath.Join("templates", tmpl),
+		}
+		t := template.Must(template.ParseFiles(files...))
+		c.Status(http.StatusOK)
+		t.ExecuteTemplate(c.Writer, "layout", data)
+	}
+	render_ := func(c *gin.Context, tmpl string, data gin.H) {
+		files := []string{
+			filepath.Join("templates", "body.html"),
+			filepath.Join("templates", tmpl),
+		}
+		t := template.Must(template.ParseFiles(files...))
+		c.Status(http.StatusOK)
+		t.ExecuteTemplate(c.Writer, "body", data)
+	}
 	// Routes
 	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "home.html", gin.H{"Title": "Home"})
+		render(c, "home.html", gin.H{
+			"Title":   "Home",
+			"ShowNav": true,
+		})
 	})
 
+
 	r.GET("/about", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "about.html", gin.H{"Title": "About"})
+		render (c, "about.html", gin.H{
+			"Title":   "Home",
+			"ShowNav": true,
+		})
 	})
 
 	r.GET("/login", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "login.html", gin.H{"Title": "Login"})
-	})
-
-	r.GET("/body", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "body.html", gin.H{"Title": "Login"})
+		render(c, "login.html", gin.H{
+			"Title":   "Login",
+			"ShowNav": false,
+		})
 	})
 
 	r.GET("/welcome", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "welcome.html", gin.H{"Title": "Login"})
+		render_(c, "welcome.html", gin.H{
+			"Title":   "Welcome",
+			"ShowNav": false,
+		})
 	})
-
-
 	r.GET("/bemvindo", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "bemvindo.html", gin.H{"Title": "Login"})
+		render_(c, "bemvindo.html", gin.H{
+			"Title":   "Bem Vindo",
+			"ShowNav": false,
+		})
 	})
 
 	r.Run(":8080")
